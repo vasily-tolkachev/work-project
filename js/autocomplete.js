d952e29d1558d626c91e33871bda9745c5175288
Vasily Tolkachev <vasilytolkachev@gmail.com>
2016-10-12 21:21:03 +0300

    Обновил файлы

---
+++ js/autocomplete.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function() {
+	document.getElementById('modal-consult').addEventListener('shown.bs.modal', function(evt) {
+		document.getElementById('consultFormTextarea').value="moo";
+	});
+	document.getElementById('hostcall-btn').onclick = function() {
+		document.getElementById('consultFormTextarea').value="Хочу заказать оборудование Hostcall";
+	}
+	document.getElementById('medbells-btn').onclick = function() {
+		document.getElementById('consultFormTextarea').value="Хочу заказать оборудование Medbells";
+	}
+	document.getElementById('medbeep-btn').onclick = function() {
+		document.getElementById('consultFormTextarea').value="Хочу заказать оборудование Medeep";
+	}
+	document.getElementById('pulsar-btn').onclick = function() {
+		document.getElementById('consultFormTextarea').value="Хочу заказать оборудование Пульсар";
+	}
+	$('#modal-consult').on('hide.bs.modal', function () {
+		document.getElementById('consultFormTextarea').value="";
+	})
+})();
